e5b1dcce8b9213e62ce492665952111145221b96
ericmschow <eric.m.schow@gmail.com>
2017-05-09 12:14:30 -0500

    added server ip

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'dev')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '45.76.235.74',
+    ]
 
 
 # Application definition
